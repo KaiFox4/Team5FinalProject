422840961f3950a88480546250cc9198150ee907
kylielerberg <148277979+kylielerberg@users.noreply.github.com>
2023-10-17 14:38:58 -0500

    Update DraftOfCode.cpp

--- DraftOfCode.cpp
+++ DraftOfCode.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+// there are 5 lights total 
+
+if(power_level <= 100 && power_level >= 80)
+{
+  // all the lights are on
+}
+else if(power_level < 80 && power_level >= 60)
+{
+  // lights 1-4 are on
+}
